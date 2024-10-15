c304ae64b3114c0e5b77fea524d6a395e7365bf4
saeruming <saeruming@gmail.com>
2024-10-15 14:18:24 +0900

    useState -> useReducer

---
+++ section09/src/components/TodoItem.jsx
@@ -0,0 +1,40 @@
+import "./TodoItem.css";
+
+const TodoItem = ({
+  id,
+  isDone,
+  content,
+  date,
+  onUpdate,
+  onDelete,
+}) => {
+  console.log("TotoItem 컴포넌트 실행");
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+    console.log("체크박스: " + id);
+  };
+
+  const onClickDelete = () => {
+    onDelete(id);
+  };
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox}
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">
+        {new Date(date).toLocaleDateString()}
+      </div>
+      <button onClick={onClickDelete}>
+        삭제
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
